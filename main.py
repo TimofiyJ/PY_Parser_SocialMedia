838f777c8bdc1393e5adef40a2997a66f86dbd86
Tim <44300490+TimofiyJ@users.noreply.github.com>
2023-08-23 17:30:22 +0300

    half-way there

--- main.py
+++ main.py
@@ -11,40 +11,54 @@ result = []
 names=[]
 data={}
 
-#def calculate_comments()
+def calculate_comments(comment_rule, author_rule, text_rule, likes_rule, source):
+    comment_section= {}
+    for i in range(len(comment_rule["class"])):
+        comments = source.find_all(f"{comment_rule['tag'][i]}",class_=f"{comment_rule['class'][i]}",id=f"{comment_rule['id'][i]}")
+        for comment in comments:
+            comment_author = comment.find(f"{author_rule['tag'][i]}",class_=f"{author_rule['class'][i]}",id=f"{author_rule['id'][i]}") if comment.find(f"{author_rule['tag'][i]}",class_=f"{author_rule['class'][i]}",id=f"{author_rule['id'][i]}")!=None else f"{i}-step-not-found"
+            if author_rule["variable"][i]!="" and comment_author!= f"{i}-step-not-found" and f"{author_rule['variable'][i]}" in comment_author.attrs:
+                comment_author = comment_author[f"{author_rule['variable'][i]}"]
+            else:
+                if comment_author.text!=None:
+                    comment_author = comment_author.text
+            comment_text = comment.find(f"{text_rule['tag'][i]}",class_=f"{text_rule['class'][i]}",id=f"{text_rule['id'][i]}") if comment.find(f"{text_rule['tag'][i]}",class_=f"{text_rule['class'][i]}",id=f"{text_rule['id'][i]}")!=None else f"{i}-step-not-found"
+            if text_rule["variable"][i]!="" and comment_text!= f"{i}-step-not-found" and f"{text_rule['variable'][i]}" in comment_text.attrs:
+                comment_text = comment_text[f"{text_rule['variable'][i]}"]
+            else:
+                if comment_text.text!=None:
+                    comment_text = comment_text.text
+            comment_likes = comment.find(f"{likes_rule['tag'][i]}",class_=f"{likes_rule['class'][i]}",id=f"{likes_rule['id'][i]}") if comment.find(f"{likes_rule['tag'][i]}",class_=f"{likes_rule['class'][i]}",id=f"{likes_rule['id'][i]}")!=None else f"{i}-step-not-found"
+            if likes_rule["variable"][i]!="" and comment_likes!= f"{i}-step-not-found" and f"{likes_rule['variable'][i]}" in comment_likes.attrs:
+                comment_likes = comment_likes[f"{likes_rule['variable'][i]}"]
+            else:
+                if comment_likes.text!=None:
+                    comment_likes = comment_likes.text
+            if comment_author!="":
+                comment_container = {}
+                comment_container[comment_text] = comment_likes
+                comment_section[comment_author] = comment_container
+    print(comment_section)
+    return comment_section
 
 def get_info(name,parameters,source):
-
-    for i in range(len(parameters["class"])): #class is just for example, every parameter should contain equal elements
-
-        if parameters["class"][i]!="": 
-
-            if parameters["variable"][i]=="":
-                name = source.find(f"{parameters['tag'][i]}",class_=f"{parameters['class'][i]}") if source.find(f"{parameters['tag'][i]}",class_=f"{parameters['class'][i]}")!=None else f"{i}-step-not-found"
-            else:
-                name = source.find(f"{parameters['tag'][i]}",class_=f"{parameters['class'][i]}") if source.find(f"{parameters['tag'][i]}",class_=f"{parameters['class'][i]}")!=None else f"{i}-step-not-found"
-                if name!= f"{i}-step-not-found" and f"{parameters['variable'][i]}" in name.attrs:
-                    name = name[f"{parameters['variable'][i]}"]
-                else:
-                    name = f"{i}-step-not-found"
+    if name=="comments":
+        return calculate_comments(parameters,parameters["comment_author"],parameters["comment_text"],parameters["comment_likes"],source)
     
-        else:
-            if parameters["variable"][i]=="":
-                name = source.find(f"{parameters['tag'][i]}",id=f"{parameters['id'][i]}") if source.find(f"{parameters['tag'][i]}",id=f"{parameters['id'][i]}")!=None else f"{i}-step-not-found"
-            else:
-                name = source.find(f"{parameters['tag'][i]}",id=f"{parameters['id'][i]}")[f"{parameters['variable'][i]}"] if source.find(f"{parameters['tag'][i]}",id=f"{parameters['id'][i]}")[f"{parameters['variable'][i]}"]!=None else f"{i}-step-not-found"
-                if name!=f"{i}-step-not-found" and f"{parameters['variable'][i]}" in name.attrs:
-                    name = name[f"{parameters['variable'][i]}"]
-                else:
-                    name = f"{i}-step-not-found"
-                    
-        if name==f"{i}-step-not-found":
+    for i in range(len(parameters["class"])): #class is just for example, every parameter should contain equal elements
+        name = source.find(f"{parameters['tag'][i]}",class_=f"{parameters['class'][i]}",id=f"{parameters['id'][i]}") if source.find(f"{parameters['tag'][i]}",class_=f"{parameters['class'][i]}",id=f"{parameters['id'][i]}")!=None else f"{i}-step-not-found"
+        if parameters["variable"][i]!="" and name!= f"{i}-step-not-found" and f"{parameters['variable'][i]}" in name.attrs:
+            name = name[f"{parameters['variable'][i]}"]
             break
-        else:
-            source=name
-    if len(parameters['variable'])>0 and parameters['variable'][-1]=="":
-        if name.text!=None:
+            #can add ere if name=="" -> name=0
+        elif parameters["variable"][i]!="" and (name== f"{i}-step-not-found" or f"{parameters['variable'][i]}" not in name.attrs):
+            name = f"{i}-step-not-found"
+        
+        if name!=f"{i}-step-not-found":
             name=name.text
+            break
+    if name.find("step-not-found")!=-1:
+        return name
     return name
 
 a1=input()
@@ -68,25 +82,15 @@ with open(f'{a2}', 'a',encoding="utf-8", errors='ignore') as file_write:
         info = {}
 
         for key in data:
-            info[key] = get_info(key,data[key],soup)  
+            #if key needs to be searched at soup:
+                #get_info(key,data[key],soup)
+            info[key] = get_info(key,data[key],post)  
 
         if info["post_link"]!=None:
             info["post_link"] = "vk.com"+info["post_link"]
-
-        comments = post.find_all("div",class_ = "reply_content")
-        comment_section= {}
-
-        for comment in comments:
-            comment_author = comment.find("a",class_ = "author").text if comment.find("a",class_ = "author")!=None else ""
-            comment_text = comment.find("div",class_ = "wall_reply_text").text if comment.find("div",class_ = "wall_reply_text")!=None else ""
-            comment_likes = comment.find("a",class_ = "like_btn")['data-count'] if comment.find("a",class_ = "like_btn")!=None else 0
-            if comment_author!="":
-                comment_container = {}
-                comment_container[comment_text] = comment_likes
-                comment_section[comment_author] = comment_container
-
-        info["comment_section"]=comment_section
-
+        
+        #data cleaning
+        info["post_views"] = str(info["post_views"]).split(" ")[0]
         print(info)
 
         quit()
